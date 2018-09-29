3038c8d0d2468220a36838c72acf69947c61061c
Seiji Osada <seiji13r@outlook.com>
2018-09-29 11:31:56 -0500

    Project Update

--- app.js
+++ app.js
@@ -66,3 +66,37 @@ firebase.initializeApp(config);
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
+
+
+//   LAST CODE EXPLAINED
+// // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+// dataRef.ref().on("child_added", function(childSnapshot) {
+
+//     // Log everything that's coming out of snapshot
+//     console.log(childSnapshot.val().name);
+//     console.log(childSnapshot.val().name);
+//     console.log(childSnapshot.val().email);
+//     console.log(childSnapshot.val().age);
+//     console.log(childSnapshot.val().comment);
+//     console.log(childSnapshot.val().joinDate);
+
+//     // full list of items to the well
+//     $("#full-member-list").append("<div class='well'><span class='member-name'> " +
+//       childSnapshot.val().name +
+//       " </span><span class='member-email'> " + childSnapshot.val().email +
+//       " </span><span class='member-age'> " + childSnapshot.val().age +
+//       " </span><span class='member-comment'> " + childSnapshot.val().comment +
+//       " </span></div>");
+
+//     // Handle the errors
+//   }, function(errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+//   });
+
+//   dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+//     // Change the HTML to reflect
+//     $("#name-display").text(snapshot.val().name);
+//     $("#email-display").text(snapshot.val().email);
+//     $("#age-display").text(snapshot.val().age);
+//     $("#comment-display").text(snapshot.val().comment);
+//   });
