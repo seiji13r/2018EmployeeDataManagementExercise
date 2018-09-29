9c8c7943586168491c12d9270369dabdad1ce730
Seiji Osada <seiji13r@outlook.com>
2018-09-29 11:07:20 -0500

    app.js update

--- app.js
+++ app.js
@@ -25,17 +25,18 @@ firebase.initializeApp(config);
     event.preventDefault();
 
     // Grabbed values from text boxes
-    name = $("#name-input").val().trim();
-    email = $("#email-input").val().trim();
-    age = $("#age-input").val().trim();
-    comment = $("#comment-input").val().trim();
+    name = $("#input-name").val().trim();
+    role = $("#input-role").val().trim();
+    startDate = $("#input-start-date").val().trim();
+    monthlyRate = $("#input-rate").val().trim();
+
 
     // Code for handling the push
     database.ref().push({
       name: name,
-      email: email,
-      age: age,
-      comment: comment,
+      role: role,
+      startDate: startDate,
+      monthlyRate: monthlyRate,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
@@ -43,20 +44,20 @@ firebase.initializeApp(config);
 
   // Firebase watcher .on("child_added"
   database.ref().on("child_added", function(snapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = snapshot.val();
+    // // storing the snapshot.val() in a variable for convenience
+    // var sv = snapshot.val();
 
-    // Console.loging the last user's data
-    console.log(sv.name);
-    console.log(sv.email);
-    console.log(sv.age);
-    console.log(sv.comment);
+    // // Console.loging the last user's data
+    // console.log(sv.name);
+    // console.log(sv.email);
+    // console.log(sv.age);
+    // console.log(sv.comment);
 
-    // Change the HTML to reflect
-    $("#name-display").text(sv.name);
-    $("#email-display").text(sv.email);
-    $("#age-display").text(sv.age);
-    $("#comment-display").text(sv.comment);
+    // // Change the HTML to reflect
+    // $("#name-display").text(sv.name);
+    // $("#email-display").text(sv.email);
+    // $("#age-display").text(sv.age);
+    // $("#comment-display").text(sv.comment);
 
     // Handle the errors
   }, function(errorObject) {
